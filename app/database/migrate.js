a36023cfd0cf353e440fdbda0d2404bc87d62f4c
Boby Ertanto <cool.ertanto@gmail.com>
2020-12-19 01:26:37 +0800

    Remove seeder, truncate tables before test start

--- app/database/migrate.js
+++ app/database/migrate.js
@@ -3,11 +3,6 @@ const TeacherModel = require('../models/teacher-model');
 const StudentModel = require('../models/student-model');
 const RegistrationModel = require('../models/registration-model');
 
-let options = {};
-if (process.env.NODE_ENV==='test'){
-  options = { force: true };
-} 
-
 (async () => {
-  await SequelizeConnection.sync(options);
+  await SequelizeConnection.sync();
 })();
