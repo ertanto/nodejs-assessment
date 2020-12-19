0c58970991c802794ac78514eefad51b06f8342f
Boby Ertanto <cool.ertanto@gmail.com>
2020-12-20 03:02:53 +0800

    Code cleanup

--- app/controllers/student/registration.js
+++ app/controllers/student/registration.js
@@ -6,9 +6,8 @@ const Util = require('../../utils/util');
 const RequiredPayloadPropertyMissingException = require('../../exceptions/required-payload-property-missing-exception');
 const InvalidPayloadDataException = require('../../exceptions/invalid-payload-data-exception');
 const InvalidEmailException = require('../../exceptions/invalid-email-exception');
-const RecordNotFoundException = require('../../exceptions/record-not-found-exception');
 
-const validatePayload = (payload) => {
+const validate = async (payload) => {
   if (typeof payload.teacher==='undefined' || typeof payload.students==='undefined' ) throw new RequiredPayloadPropertyMissingException("Required payload property missing");
   if (typeof payload.teacher!=='string' || typeof payload.students!=='object') throw new InvalidPayloadDataException("Invalid payload data");
   if (!Util.validEmail(payload.teacher)) throw new InvalidEmailException("Invalid email in the payload data");
@@ -22,28 +21,25 @@ const validatePayload = (payload) => {
 module.exports = async (request, reply) => {
   try{
     const payload = Util.parseData(request.payload);  
-    validatePayload(payload);
+    await validate(payload);
 
     try{
-      let teacherModel = await TeacherModel.findOne({ where: { email: payload.teacher } });
-      if (teacherModel===null){
-        teacherModel = await TeacherModel.create({ email: payload.teacher } );
+      let teacher = await TeacherModel.findOne({ where: { email: payload.teacher } });
+      if (teacher===null){
+        teacher = await TeacherModel.create({ email: payload.teacher } );
       }
       for(let i=0; i<payload.students.length; i++){
-        let studentModel = await StudentModel.findOne({ where: { email: payload.students[i] } });
-        if (studentModel===null){
-          studentModel = await StudentModel.create({ email: payload.students[i] } );
+        let student = await StudentModel.findOne({ where: { email: payload.students[i] } });
+        if (student===null){
+          student = await StudentModel.create({ email: payload.students[i] } );
         }
 
-        let registrationModel = await RegistrationModel.findOne({ where: { teacher_id: teacherModel.id, student_id: studentModel.id } });
+        let registrationModel = await RegistrationModel.findOne({ where: { teacher_id: teacher.id, student_id: student.id } });
         if (registrationModel===null){
-          await RegistrationModel.create({ teacher_id: teacherModel.id, student_id: studentModel.id });
+          await RegistrationModel.create({ teacher_id: teacher.id, student_id: student.id });
         }
       }
     } catch (error) {
-      if (error instanceof RecordNotFoundException){
-        throw new RecordNotFoundException(error.message);
-      }
       throw new Error("Something went wrong");
     }
 
